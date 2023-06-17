fbd7924eb8800611ad941196187cade52ac96336
Wai San Gu <waisangu@gmail.com>
2023-06-17 10:12:34 -0400

    prog

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 const pinRouter = require('./router/pinRouter.js');
-// const PORT = 8080;
+const PORT = 3000;
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/router', pinRouter);
+
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+  
 
 app.listen(PORT, () => {
     console.log(`Server listening on Port: ${PORT}`);
