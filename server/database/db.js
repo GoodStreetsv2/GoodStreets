3ec2c75acc7aa92a23bdf7edf43c1674147be3f5
Wai San Gu <waisangu@gmail.com>
2023-06-16 20:59:22 -0400

    progress

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,6 @@ const { Pool } = require('pg');
 
 const pool = new Pool({connectionString});
 
-
 module.exports = {
     async query(text, params) {
       const res = await pool.query(text, params);
--- server/server.js
+++ server/server.js
@@ -1,30 +1,13 @@
 const express = require('express');
 const app = express();
-const pinRouter = require('./router/PinRouter.js');
+const pinRouter = require('./router/pinRouter.js');
 // const PORT = 8080;
 
-
-//we need to 'npm install pg'
-// const db = pg(connectionString);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/router', pinRouter);
-// var conString = "postgres://lnbgaogn:c3SKWHGnWIpZcTYbWbkQRQuElzLnJJeq@rajje.db.elephantsql.com/lnbgaogn" //Can be found in the Details page
-// var client = new pg.Client(conString);
-// client.connect(function(err) {
-//   if(err) {
-//     return console.error('could not connect to postgres', err);
-//   }
-//   client.query('SELECT NOW() AS "theTime"', function(err, result) {
-//     if(err) {
-//       return console.error('error running query', err);
-//     }
-//     console.log(result.rows[0].theTime);
-//     client.end();
-//   });
-// });
 
 app.listen(PORT, () => {
     console.log(`Server listening on Port: ${PORT}`);
